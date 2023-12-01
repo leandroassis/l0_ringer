c3751f02fd1c9376f15004836a9c18cd6e102902
Leandro Assis dos Santos <leandro.santos@login-server.lps.ufrj.br>
2023-12-01 11:24:27 -0300

    adição de script para criar job antes

--- scripts/convert_eds.sh
+++ scripts/convert_eds.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 paths=("/eos/user/e/eegidiop/lorenzettiCom/datasets/zee/prod0000.092023.10k.nopileup.V0/ESD/Zee.ESD.root.0" \
-         "/eos/user/e/eegidiop/lorenzettiCom/datasets/zee/prod0001.102023.50k.nopileup.V0/ESD/zee.ESD.root"   \
-         "/eos/user/e/eegidiop/lorenzettiCom/datasets/zee/prod0002.102023.100k.nopileup.V0/ESD/zee.ESD.root"  \
-         "/eos/user/e/eegidiop/lorenzettiCom/datasets/zee/prod0003.102023.100k.nopileup.V0/ESD/zee.ESD.root"  \
-         "/eos/user/e/eegidiop/lorenzettiCom/datasets/zee/prod0004.102023.100k.nopileup.V0/ESD/zee.ESD.root")
+       "/eos/user/e/eegidiop/lorenzettiCom/datasets/zee/prod0001.102023.50k.nopileup.V0/ESD/zee.ESD.root"   \
+       "/eos/user/e/eegidiop/lorenzettiCom/datasets/zee/prod0002.102023.100k.nopileup.V0/ESD/zee.ESD.root"  \
+       "/eos/user/e/eegidiop/lorenzettiCom/datasets/zee/prod0003.102023.100k.nopileup.V0/ESD/zee.ESD.root"  \
+       "/eos/user/e/eegidiop/lorenzettiCom/datasets/zee/prod0004.102023.100k.nopileup.V0/ESD/zee.ESD.root"
+       )
 
 cd $HOME/l0_ringer/src/eds_parser
 mkdir $HOME/l0_ringer/data/
 
 for i in ${!paths[@]}; do
     mkdir $HOME/l0_ringer/data/data_$i
-    nohup python3 $HOME/l0_ringer/src/eds_parser/read_eds.py -i ${paths[$i]} -o $HOME/l0_ringer/data/data_$i/readout.csv >> $HOME/l0_ringer/data/data_$i/log.txt &
+    mkdir $HOME/l0_ringer/data/data_$i/jobs
+    python3 $HOME/l0_ringer/src/eds_parser/create_jobs.py -i ${paths[$i]} -o $HOME/l0_ringer/data/data_$i/cells.csv -j $HOME/l0_ringer/data/data_$i/jobs/
+    nohup python3 $HOME/l0_ringer/src/eds_parser/convert_eds.py -j $HOME/l0_ringer/data/data_$i/jobs/ > $HOME/l0_ringer/data/data_$i/log.txt &
 done
