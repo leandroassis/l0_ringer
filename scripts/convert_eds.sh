6646b6c592e51e8cb299d1ce5ee2293c40dd4cae
Leandro Assis dos Santos <leandro.santos@login-server.lps.ufrj.br>
2023-12-01 11:34:27 -0300

    corrigindo nome script de tune jobs

--- scripts/convert_eds.sh
+++ scripts/convert_eds.sh
@@ -14,5 +14,5 @@ for i in ${!paths[@]}; do
     mkdir $HOME/l0_ringer/data/data_$i
     mkdir $HOME/l0_ringer/data/data_$i/jobs
     python3 $HOME/l0_ringer/src/eds_parser/create_jobs.py -i ${paths[$i]} -o $HOME/l0_ringer/data/data_$i/cells.csv -j $HOME/l0_ringer/data/data_$i/jobs/
-    nohup python3 $HOME/l0_ringer/src/eds_parser/convert_eds.py -j $HOME/l0_ringer/data/data_$i/jobs/ > $HOME/l0_ringer/data/data_$i/log.txt &
+    nohup python3 $HOME/l0_ringer/src/eds_parser/read_eds.py -j $HOME/l0_ringer/data/data_$i/jobs/ > $HOME/l0_ringer/data/data_$i/log.txt &
 done
