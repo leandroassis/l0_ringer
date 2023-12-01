b4f70add5e199270d858c5b057c05924eb7f6323
Leandro Assis dos Santos <leandro.santos@login-server.lps.ufrj.br>
2023-11-30 22:45:15 -0300

    corrigindo importação de pacotes

--- src/eds_parser/read_eds.py
+++ src/eds_parser/read_eds.py
@@ -4,8 +4,8 @@ import ROOT
 
 import sys
 # caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, '../lorenzetti_utils/')
-sys.path.insert(1, '../')
+#sys.path.insert(1, '../lorenzetti_utils/')
+#sys.path.insert(1, '../')
 
 from lorenzetti_utils.EventStore import EventStore
 import argparse
