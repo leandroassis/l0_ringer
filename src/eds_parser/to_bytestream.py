7f87d3df7d5f53e999d10e6668c6f7b816a038b3
Leandro Assis dos Santos <leandro.santos@login-server.lps.ufrj.br>
2023-12-02 03:35:27 -0300

    removendo ieração por .csv dir; corrigindo extensao gzip; adicionando limitadores de eventos

--- src/eds_parser/to_bytestream.py
+++ src/eds_parser/to_bytestream.py
@@ -8,10 +8,10 @@ import os
 import argparse
 import tqdm
 
-# usage: python3 to_bytestream.py -c <path to .csv dir> -o <base path for hex file>
-# example: python3 to_bytestream.py -c /eos/user/l/lassisdo/eds_parser/02-12-2023/data/data_0/events -o /eos/user/l/lassisdo/eds_parser/02-12-2023/data/data_0/hex
+# usage: python3 to_bytestream.py -c <path to .csv> -o <base path for hex file>
+# example: python3 to_bytestream.py -c /eos/user/l/lassisdo/eds_parser/02-12-2023/data/data_0/events/xxxx.csv -o /eos/user/l/lassisdo/eds_parser/02-12-2023/data/data_0/hex
 
-def csv_to_hex(csv_path, hex_path):
+def csv_to_hex(csv_path, hex_path, start, end):
     """
     Read a csv file and convert it to a hex file.
     """
@@ -33,12 +33,15 @@ def csv_to_hex(csv_path, hex_path):
     with tqdm.tqdm(total=len(df['entry_idx'].unique())) as pbar:
         for entry_idx in df['entry_idx'].unique():
 
+            if entry_idx < start or entry_idx >= end:
+                continue
+
             with open(hex_path+"event_%05d.hex" %entry_idx, 'wb') as hex_file:
 
                 cells_from_entry = df[keys].loc[df['entry_idx'] == entry_idx]
 
                 for cell_idx in range(len(cells_from_entry)):
-                    pbar.set_description("Writing cell %05d/%05d from event %05d." %(cell_idx, len(cells_from_entry), entry_idx))
+                    pbar.set_description_str("Writing cell %d/%d from event %d." %(cell_idx, len(cells_from_entry), entry_idx))
                     for key in keys:
                         if key in keys[:-2]: # se a key estiver entre eta e sampling (não incluindo sampling)
                             hex_file.write(cells_from_entry[key].iloc[cell_idx].astype('float32').tobytes())
@@ -46,8 +49,8 @@ def csv_to_hex(csv_path, hex_path):
                             hex_file.write(cells_from_entry[key].iloc[cell_idx].astype('uint32').tobytes())
             
             # compress hex file
-            compress_file(hex_path+"event_%05d.hex" %entry_idx)
-            pbar.set_description("Compressing event %05d." %entry_idx)
+            compress_file(hex_path+"event_%d.hex" %entry_idx)
+            pbar.set_description_str("Compressing event %d." %entry_idx)
             pbar.update(1)
     
     # compress csv file
@@ -59,7 +62,7 @@ def compress_file(file_path):
     Compress a file using gzip.
     """
     with open(file_path, 'rb') as f_in:
-        with gzip.open(file_path+'.gpg', 'wb') as f_out:
+        with gzip.open(file_path+'.gz', 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
     
     # delete uncompressed file
@@ -70,20 +73,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert csv to hex and compress both the files.')
     parser.add_argument('-c', '--csv_path', type=str, help='Path to csv file.', required=True)
     parser.add_argument('-o', '--out_path', type=str, help='Path to output file.', required=True)
+    parser.add_argument('-s', '--start', type=int, help='Start index of the file.', required=False, default=0)
+    parser.add_argument('-e', '--end', type=int, help='End index of the file.', required=False, default=0)
 
     arguments = parser.parse_args()
 
-    if arguments.csv_path[-1] != '/':
-        arguments.csv_path += '/'
-
     if arguments.out_path[-1] != '/':
         arguments.out_path += '/'
-
-    if not os.path.exists(arguments.out_path):
-        os.makedirs(arguments.out_path)
-
-    for filename in os.scandir(arguments.csv_path):
-        if not filename.is_file() or filename.name[-4:] != ".csv":
-            continue
         
-        csv_to_hex(filename.path, arguments.out_path)
+    csv_to_hex(arguments.csv_path, arguments.out_path, arguments.start, arguments.end)
